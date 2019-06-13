bc21d6be4044827453272c801c42f8e21390d6e5
b05902046 <b05902046@ntu.edu.tw>
2019-06-13 11:14:03 +0800

    g

--- connect_fastest.py
+++ connect_fastest.py
@@ -115,7 +115,7 @@ class traverse:
 
         name = wifi['ESSID']
         #print('name: {}'.format(name))
-        
+        print("try {}".format(name))
         # check authentication suites
         if 'Authentication Suites (1)' in wifi:
             authentication = wifi['Authentication Suites (1)']
